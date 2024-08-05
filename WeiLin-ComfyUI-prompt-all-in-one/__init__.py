17138d887491513d134cde2de834b27d4d97815e
WeiLin <49275176+weilin9999@users.noreply.github.com>
2024-08-05 17:56:00 +0800

    Add files via upload
    文件初始化

---
+++ WeiLin-ComfyUI-prompt-all-in-one/__init__.py
@@ -0,0 +1,97 @@
+
+import re
+import random
+
+#正向提示词
+class WeiLinComfyUIPromptAllInOneGreat:
+
+    def __init__(self):
+        pass
+    
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "text": ("STRING", {
+                    "multiline": True, #True if you want the field to look like the one on the ClipTextEncode node
+                    "default": "",
+                    "placeholder": "输入提示词",
+                    "id": "weilin_great_txt2img_prompt"
+                }),
+            },
+        }
+
+    RETURN_TYPES = ("STRING",)
+    #RETURN_NAMES = ("image_output_name",)
+
+    FUNCTION = "encode"
+
+    #OUTPUT_NODE = False
+
+    CATEGORY = "conditioning"
+
+    def encode(self, text):
+        text= extract_tags(text)
+        return (text,)
+    
+
+#反向提示词
+class WeiLinComfyUIPromptAllInOneNeg:
+
+    def __init__(self):
+        pass
+    
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "text": ("STRING", {
+                    "multiline": True, #True if you want the field to look like the one on the ClipTextEncode node
+                    "default": "",
+                    "placeholder": "输入提示词",
+                    "id": "weilin_neg_txt2img_prompt"
+                }),
+            },
+        }
+
+    RETURN_TYPES = ("STRING",)
+    #RETURN_NAMES = ("image_output_name",)
+
+    FUNCTION = "encode"
+
+    #OUTPUT_NODE = False
+
+    CATEGORY = "conditioning"
+
+    def encode(self, text):
+        text= extract_tags(text)
+        return (text,)
+
+def extract_tags(text):
+    pattern = r'#\[(.*?)\]'
+    matches=re.findall(pattern, text)  
+    for i in matches:
+        newarr=i.split(',')
+        random.seed(random.random())
+        rdindex=random.randint(0,len(newarr)-1)
+        rdtext=newarr[rdindex]
+        text = re.sub(pattern, rdtext, text,count=1)
+    return text
+
+
+# A dictionary that contains all nodes you want to export with their names
+# NOTE: names should be globally unique
+NODE_CLASS_MAPPINGS = {
+    "WeiLinComfyUIPromptAllInOneGreat": WeiLinComfyUIPromptAllInOneGreat,
+    "WeiLinComfyUIPromptAllInOneNeg": WeiLinComfyUIPromptAllInOneNeg
+}
+
+# A dictionary that contains the friendly/humanly readable titles for the nodes
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "WeiLinComfyUIPromptAllInOneGreat": "WeiLin PromptAllInOne 正向提示词",
+    "WeiLinComfyUIPromptAllInOneNeg": "WeiLin PromptAllInOne 反向提示词"
+}
+
+WEB_DIRECTORY = "./js"
+
+__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
---
+++ WeiLin-ComfyUI-prompt-all-in-one/js/weiLinComfyUIPromptAllInOneNeg.js
@@ -0,0 +1,201 @@
+import { app } from '../../scripts/app.js'
+
+class PromptUINode {
+  timer = null;
+  constructor(devObj) {
+    this.devObj = devObj;
+    this.makeElements();
+  }
+
+  makeElements() {
+    const panelPaintBox = document.createElement("div");
+    panelPaintBox.innerHTML = `
+    <div id="weilin_neg_prompt_in_all_one_widget" class="weilin_neg_prompt_in_all_one_widget">
+      <button id="weilin_neg_propmptUI_open" >打开PromptUI</button>
+    </div>
+    `;
+    // Main panelpaint box
+    panelPaintBox.className = "panelPaintBox";
+    this.panelPaintBox = panelPaintBox;
+    this.devObj.appendChild(panelPaintBox);
+    
+    const iftramBox = document.createElement("div");
+    iftramBox.innerHTML = `
+      <iframe
+        class="weilin_iframe_box"
+        id='weilin_prompt_great_box'
+        name='weilin_prompt_great_box'
+        src='http://localhost:17861/?type=neg_prompt'
+        frameborder='0'
+        scrolling='on'
+        >
+      </iframe>
+    `;
+    iftramBox.id = "weilin_bg_box_neg"
+    iftramBox.className = "weilin_bg_box"
+    iftramBox.style.display = "none"
+    document.body.appendChild(iftramBox);
+
+    this.bindEvents();
+  }
+
+  bindEvents() {
+    
+    window.addEventListener('message', e => {
+      if(e.data.handel == 'changeWeiLinNegPrompt')
+      {
+        const textarea = document.getElementById('weilin_neg_txt2img_prompt')
+        textarea.value = e.data.value
+      }else if(e.data.handel  == 'closeWeilinNegPromptBox'){
+        const textarea = document.getElementById('weilin_neg_txt2img_prompt')
+        textarea.readOnly = false
+        const ifreamBox = document.getElementById('weilin_bg_box_neg')
+        ifreamBox.style.display = "none"
+      }else if(e.data.handel == 'openWeilinNegPromptBox'){
+        const textarea = document.getElementById('weilin_neg_txt2img_prompt')
+        textarea.readOnly = true
+        for (let index = 0; index < window.length; index++) {
+          window[index].postMessage({handel: 'changeWeiLinNegPromptSD',value: textarea.value}, "*");
+        }
+        const ifreamBox = document.getElementById('weilin_bg_box_neg')
+        ifreamBox.style.display = "flex"
+      }
+    }, false);
+  }
+}
+
+
+function WeiLinPromptUIWidget(node, inputName, inputData, app) {
+  node.name = inputName;
+  const widget = {
+    type: "weilin_prompt_in_all_one_neg_widget",
+    name: `w${inputName}`,
+    callback: () => {},
+    draw: function (ctx, _, widgetWidth, y, widgetHeight) {
+      const margin = 10,
+        left_offset = 0,
+        top_offset = 0,
+        visible = app.canvas.ds.scale > 0.6 && this.type === "weilin_prompt_in_all_one_neg_widget",
+        w = widgetWidth - margin * 2 - 10,
+        clientRectBound = ctx.canvas.getBoundingClientRect(),
+        transform = new DOMMatrix()
+          .scaleSelf(
+            clientRectBound.width / ctx.canvas.width,
+            clientRectBound.height / ctx.canvas.height
+          )
+          .multiplySelf(ctx.getTransform())
+          .translateSelf(margin, margin + y),
+        scale = new DOMMatrix().scaleSelf(transform.a, transform.d);
+
+      Object.assign(this.painter_wrap.style, {  //auto scale
+        left: `${transform.a * margin * left_offset + transform.e}px`,
+        top: `${transform.d + transform.f + top_offset}px`,
+        width: `${w * transform.a}px`,
+        position: "absolute",
+        zIndex: app.graph._nodes.indexOf(node),
+      });
+
+      Object.assign(this.painter_wrap.children[0].style, { 
+        transformOrigin: "0 0",
+        transform: scale,
+        width: w + "px",
+      });
+      // this.painter_wrap.hidden = !visible;   // no hidden
+    },
+  };
+  let devElmt = document.createElement("div");
+  node.capture = new PromptUINode(devElmt);
+  widget.painter_wrap = node.capture.devObj;
+
+  widget.parent = node;
+
+  // node.capture.makeElements();
+  document.body.appendChild(widget.painter_wrap);
+
+  node.addCustomWidget(widget);
+  node.onRemoved = () => {
+    const textarea = document.getElementById('weilin_neg_txt2img_prompt')
+    textarea.remove();
+    for (let y in node.widgets) {
+      if (node.widgets[y].painter_wrap) {
+        node.widgets[y].painter_wrap.remove();
+        clearInterval(node.widgets[y].painter_wrap.timer); 
+      }
+    }
+  };
+  node.onResize = function () {
+    let [w, h] = this.size;
+    if (w <= 501) w = 500;
+    if (h <= 201) h = 200;
+
+    if (w > 501) {
+      h = w + 40;
+    }
+    this.size = [w, h];
+  };
+
+  return { widget: widget };
+}
+
+app.registerExtension({
+  name: "weilin.prompt_node_neg",
+  async init(app) {
+    const style = document.createElement("style");
+    style.innerText = `.panelWeiLinPrompNegBox {
+      width: 100%;
+    }
+      .weilin_bg_box{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: fixed;
+        width: 100%;
+        height: 100%;
+        left: 0;
+        top: 0;
+        z-index: 999;
+      }
+      .weilin_iframe_box{
+        min-width: 80%;
+        min-height: 80%;
+        border-radius: 20px;
+      }
+    `;
+    document.head.appendChild(style)
+
+  },
+  async setup(app) {
+
+  },
+
+
+  async beforeRegisterNodeDef(nodeType, nodeData, app) {
+    // console.log(nodeData.name)
+    if (nodeData.name === "WeiLinComfyUIPromptAllInOneNeg") {
+      // console.log(nodeData)
+      // Create node
+      const onNodeCreated = nodeType.prototype.onNodeCreated;
+      nodeType.prototype.onNodeCreated = async function () {
+        const r = onNodeCreated
+          ? onNodeCreated.apply(this, arguments)
+          : undefined;
+
+        let EasyCaptureNode = app.graph._nodes.filter(
+            (wi) => wi.type == "WeiLinComfyUIPromptAllInOneNeg"
+          );
+        // console.log(`Create WeiLinComfyUIPromptAllInOneNeg: ${nodeName}`);
+          WeiLinPromptUIWidget.apply(this, [this, '', {}, app]);
+
+
+          const openButton = document.getElementById("weilin_neg_propmptUI_open");
+          openButton.onclick = function () {
+            for (let index = 0; index < window.length; index++) {
+              window[index].postMessage({handel: 'openWeiLinNegPromptSD'}, "*");
+            }
+          }
+
+        return r;
+      };
+    }
+  },
+});
